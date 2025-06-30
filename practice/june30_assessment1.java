63437e6944b9a9ea68b44cb1334a1da2a9aefe45
jeremiahdy55 <jeremiahjava55@gmail.com>
2025-06-30 04:51:08 -1000

    add june_30 assessment1 practice

---
+++ practice/june30_assessment1.java
@@ -0,0 +1,27 @@
+package practice;
+
+public class june30_assessment1 {
+
+    public static void main(String[] args) {
+        int[] intArray = {2,4,13,15,7,8,9};
+        int k = 17;
+        String pairs = findPairsThatSumK(intArray, k);
+        System.out.println("These are the found pairs:");
+        System.out.println(pairs);
+
+    }
+
+    // Task: find pairs on integer array whose sum is equal to k
+    public static String findPairsThatSumK(int[] arr, int k) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[i] + arr[j] == k) {
+                    sb.append(String.format("(%d, %d)\n", arr[i], arr[j]));
+                }
+            }
+        }
+        return sb.toString();
+    }
+    
+}
